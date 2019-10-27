00a35c9a3c63db58bc2c728703458728295ba6df
ahmed alhalfa <ah.alhalfa@gmail.com>
2019-10-27 11:12:08 +0200

    fixed some bugs

---
+++ zork/__main__.py
@@ -0,0 +1,14 @@
+import sys
+from modules import animals, events, places, utils
+from data import ape, bear, gorilla, lion, UI_Intro
+
+
+"""
+    This game simulates zork and adventure, hope you find it funny
+"""
+
+
+
+
+if __name__ == "__main__" :
+    utils.start()
